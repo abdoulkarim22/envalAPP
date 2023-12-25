bde5e397d787f2f9b4eda55a9ad5be776c227642
abdoul karim <abdoulkarimsoumana62@gmail.com>
2023-12-25 15:40:52 +0100

    modifier

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const input_text = document.getElementById("input-text");
 const input_password = document.getElementById("input-password");
-const alert_text = document.querySelector(".alert-text")
+const alert_text = document.querySelector(".alert-text");
+const btnConnexion = document.getElementById("btnConnexion");
 
 
 
@@ -20,22 +21,87 @@ const alert_text = document.querySelector(".alert-text")
 
 
 
-function login() {
 
-compte.forEach(element => {
-    if (element.username === input_text.value && element.password === input_password.value) {
-        window.location.href = "dashboard.html";
-        console.log(false);
-    }
-    else{
-        console.log(true);
-        alert_text.classList.add("alert-text-show");
-        setTimeout (() => {
-            alert_text.classList.remove("alert-text-show");
-        },2000);
-    }
+
+
+
+// let localStorageArray = JSON.parse(localStorage.getItem("arrayLogin"));
+  let userConnect = JSON.parse(localStorage.getItem("utilisateur"));
+
+  let numberofTentatives = 0;
+ btnConnexion.addEventListener('click',function () {
+    numberofTentatives++;
+    console.log(numberofTentatives);
+     compte.forEach((element) => {
+         if (element.username === input_text.value && element.password === input_password.value) {
+             let Login = {
+              userName: element.username,
+               passWord: element.password,
+            };
+            localStorage.setItem("utilisateur", JSON.stringify(Login));
+            window.location.href = "dashboard.html";
+            numberofTentatives = 0;
+         }
+         else{
+             alert_text.classList.add("alert-text-show"); 
+             setTimeout (() => {
+                alert_text.classList.remove("alert-text-show");
+            },2000);
+         }
+         if (numberofTentatives === 3) {
+            const leSecret = prompt("Quel est Le surnom de NUEVE");
+            if (leSecret === "Moutton") {
+                window.location.href = "dashboard.html";
+            }
+         }
+        });
+    })
+//         // {
+            
+//         //     alert_text.classList.add("alert-text-show");
+//         //    setTimeout (() => {
+//         //       alert_text.classList.remove("alert-text-show");
+//         //     },2000);
+//         // }
+//     });
+ 
+// 
+
+
+// let numberofTentatives = 0;
+// function clique() {
+//     if (numberofTentatives >= 3) {
+//         alert("impossible");
+//     }else{
+//         alert("possible");
+//     }
+// }
+
+// let localStorageArray = JSON.parse(localStorage.getItem("arrayLogin"));
+// let userConnect = JSON.parse(localStorage.getItem("utilisateur"));
+
+
+// function login() {
+//     //  numberofTentatives++;
+//     // clique();
+//   compte.forEach((element,numberofTentatives) => {
+//     if (element.username === input_text.value && element.password === input_password.value) {
+//         let objLogin = {
+//             id: element.id,
+//             userName: element.username,
+//             passWord: element.password,
+//           };
+//           localStorage.setItem("utilisateur", JSON.stringify(objLogin));
+//           window.location.href = "dashboard.html";
+//     }
+//     else{
+       
+//         alert_text.classList.add("alert-text-show");
+//         setTimeout (() => {
+//             alert_text.classList.remove("alert-text-show");
+//         },2000);
+
+//     }
     
-   });
-}
-
-
+//    });
+// }
--- data.js
+++ data.js
@@ -1,23 +1,11 @@
-const compte = [
-    {
-        id: 1,
-        username: "abdoulkarim",
-        password:"codeloccol"
-    },
-    {
-        id: 2,
-        username: "Cesar",
-        password:"0000"
-    },
-    {
-        id: 3,
-        username: "admin",
-        password:"1234",
-    }
-]
+ const compte = [
+     {
+         username: "Cesar",
+         password:"0000"
+     }
+ ]
 
-const myCompte = JSON.parse(localStorage.getItem("identifient"));
-if (!myCompte) {
-    localStorage.setItem("identifient",JSON.stringify(compte))
-}
-console.log(myCompte);
+ const myCompte = JSON.parse(localStorage.getItem("identifient"));
+ if (!myCompte) {
+     localStorage.setItem("identifient",JSON.stringify(compte))
+ }
