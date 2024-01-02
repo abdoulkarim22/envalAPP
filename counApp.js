3f040eca80d7bd478cbc3d8e969ff757fdb607ba
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-01-02 22:47:06 +0100

    first commit

---
+++ counApp.js
@@ -0,0 +1,28 @@
+const file = document.querySelector("#input-file"),
+  photo = document.querySelector("#photo"),
+  navarImg = document.querySelector("#navar-img"),
+  btnDelete = document.querySelector("#btn-delete"),
+  inputName = document.querySelector("#input-name"),
+  inputEmail = document.querySelector("#input-email"),
+  inputBiographie = document.querySelector("textarea"),
+  btnSend = document.querySelector(".btn-send"),
+  alert1 = document.querySelector(".alert-text"),
+  alert2 = document.querySelector(".alert-text1"),
+  alert3 = document.querySelector(".alert-text2"),
+  alert4 = document.querySelector(".alert-text3"),
+  btnUpdateProfil = document.querySelector("#btn-updateProfil"),
+  btnDeleteProfil = document.getElementById(".btn-deleteProfil"),
+  oldPassword = document.querySelector("#old-password"),
+  newPassword = document.querySelector("#new-password"),
+  confirmationPassword = document.querySelector("#confirmation-password"),
+  btnSend1 = document.querySelector("#btnSend");
+
+
+//   btnDeleteProfil.addEventListener('click',function () {
+//     alert4.classList.add("alert-text-show3");
+//   });
+
+function editMotdepasse() {
+ alert4.classList.add("alert-text-show3"); 
+
+}
